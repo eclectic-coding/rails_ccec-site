11a6c6c419ba8f7d293ea83902cdf59b043b9883
Chuck <chuck@eclecticsaddlebag.com>
2023-06-25 09:08:49 -0400

    Update tests

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ end
 
 group :test do
   gem "simplecov", "~> 0.21.2", require: false
+  gem "webmock"
 end
 
 group :development do
--- spec/helpers/avatar_helper_spec.rb
+++ spec/helpers/avatar_helper_spec.rb
@@ -3,8 +3,11 @@ require "rails_helper"
 RSpec.describe AvatarHelper, type: :helper do
   describe "returns users avatar" do
     it "builds url" do
-      user = create(:user)
-      expect(helper.avatar_path(user)).to eq("https://secure.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=180&d=mp")
+      stub_request(:get, "https://secure.gravatar.com/avatar/")
+        .with(headers: { "Accept" => "*/*", "User-Agent" => "Ruby" })
+        .to_return(status: 200, body: "stubbed response", headers: {})
+
+      expect(helper.avatar_path(create(:user))).to be_instance_of(String)
     end
   end
 end
