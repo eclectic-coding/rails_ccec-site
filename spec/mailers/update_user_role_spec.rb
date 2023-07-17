a325e7f5d8111c95872d0f419e4d18acc4ff9e6f
Chuck <chuck@eclecticsaddlebag.com>
2023-07-17 17:40:39 -0400

    Add mailer and tests for update user role

--- spec/mailers/update_user_role_spec.rb
+++ spec/mailers/update_user_role_spec.rb
@@ -1,23 +1,23 @@
 require "rails_helper"
 
-RSpec.describe UpdateUsersEmailMailer, type: :mailer do
+RSpec.describe UpdateUserRoleMailer, type: :mailer do
   include ActiveJob::TestHelper
 
   describe "Notify user of email address change" do
     specify do
       user = create(:user)
-      mail = described_class.with(user: user).update_user_email
+      mail = described_class.with(user: user).update_user_role
 
-      expect(mail.subject).to eq("Email Update")
+      expect(mail.subject).to eq("Role Update")
       expect(mail.to).to eq([user.email])
     end
 
     it "sends invitation email immediately" do
       user = create(:user)
-      user.email = "new@example.com"
+      user.add_role(:admin)
 
       expect {
-        described_class.with(user: user).update_user_email.deliver_now
+        described_class.with(user: user).update_user_role.deliver_now
       }.to change { ActionMailer::Base.deliveries.size }.by(1)
     end
   end
