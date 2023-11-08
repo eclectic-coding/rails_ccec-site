353ad7ab3684c59c6c7227e525fc72af9bb6903e
Chuck <chuck@eclecticsaddlebag.com>
2023-11-08 12:08:45 -0500

    Dropdown actions (#36)
    * Add mouseover info menu toggle
    * Upgrade ccsbundling
    * Finished up admin menu toggle

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,8 +5,6 @@
 
 import "@hotwired/turbo-rails"
 require("@rails/activestorage").start()
-//require("trix")
-//require("@rails/actiontext")
 require("local-time").start()
 require("@rails/ujs").start()
 
