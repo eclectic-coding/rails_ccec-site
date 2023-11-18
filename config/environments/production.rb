7a075396403260bf7adf1a5a3d54fda7dabc812e
Chuck <chuck@eclecticsaddlebag.com>
2023-11-18 07:48:47 -0500

    Work on mailers (#47)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ccec_basic_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.smtp_settings = {
+    address: Rails.credentials.dig(:brevo, :smtp_host),
+    port: ENV.fetch("SMTP_PORT", "587"),
+    authentication: :plain,
+    user_name: Rails.credentials.dig(:brevo, :smtp_username), # See: https://app.brevo.com/settings/keys/smtp
+    password: Rails.credentials.dig(:brevo, :smtp_password),
+    enable_starttls_auto: true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
