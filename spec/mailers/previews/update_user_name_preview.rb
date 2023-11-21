b645377a9cd439a9f7ee59c982334e492026b3a7
Chuck <chuck@eclecticsaddlebag.com>
2023-11-21 13:33:26 -0500

    Add sidekiq routes

--- spec/mailers/previews/update_user_name_preview.rb
+++ spec/mailers/previews/update_user_name_preview.rb
@@ -1,3 +1,7 @@
-# Preview all emails at http://localhost:3000/rails/mailers/update_user_name
+# Preview all emails at https://localhost:3000/rails/mailers/update_user_name
 class UpdateUserNamePreview < ActionMailer::Preview
+  def update_user_name
+    user = create(:user, name: "Fred Smith")
+    UpdateUserNameMailer.with(user: user).update_user_name
+  end
 end
