34cd841c74829d1251b85b55243d383a82282f75
Chuck <chuck@eclecticsaddlebag.com>
2023-12-13 15:29:41 -0500

    Add turbo inserted new recipient form

--- app/controllers/admin/message_recipients_controller.rb
+++ app/controllers/admin/message_recipients_controller.rb
@@ -1,9 +1,13 @@
 class Admin::MessageRecipientsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_message_recipient, only: [:edit, :update, :destroy]
+
   def index
     @message_recipients = MessageRecipient.all
   end
 
   def new
+    @message_recipient = MessageRecipient.new
   end
 
   def create
@@ -16,5 +20,15 @@ class Admin::MessageRecipientsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def message_recipient_params
+    params.require(:message_recipient).permit(:name, :email)
+  end
+
+  def set_message_recipient
+    @message_recipient = MessageRecipient.find(params[:id])
   end
 end
