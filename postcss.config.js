2ac96f77332c6d34482747e807ef53041042382f
Chuck <chuck@eclecticsaddlebag.com>
2024-01-26 09:30:55 -0500

    Update styling

--- postcss.config.js
+++ postcss.config.js
@@ -1,30 +1,8 @@
-let environment = {
+module.exports = {
     plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
         require('postcss-import'),
-        require('postcss-flexbugs-fixes'),
-        require('postcss-preset-env')({
-            autoprefixer: {
-                flexbox: 'no-2009'
-            },
-            stage: 3
-        }),
+        require('tailwindcss/nesting'),
+        require('autoprefixer'),
+        require('tailwindcss')
     ]
 }
-
-// Only run PurgeCSS in production (you can also add staging here)
-if (process.env.RAILS_ENV === "production") {
-    environment.plugins.push(
-        require('@fullhuman/postcss-purgecss')({
-            content: [
-                './app/**/*.html.haml',
-                './app/helpers/**/*.rb',
-                './app/javascript/**/*.js'
-            ],
-            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-        })
-    )
-}
-
-module.exports = environment
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
-// See the Tailwind default theme values here:
-// https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-const colors = require('tailwindcss/colors')
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
     content: [
         './app/views/**/*.html.erb',
+        './app/views/**/*.html.haml',
         './app/components/**/*.html.erb',
+        './app/components/**/*.html.haml',
         './app/helpers/**/*.rb',
         './app/assets/stylesheets/**/*.css',
         './app/javascript/**/*.js',
@@ -16,20 +13,5 @@ module.exports = {
         require('flowbite/plugin')
     ],
     theme: {
-        extend: {
-            colors: {
-                primary: colors.red,
-                secondary: colors.indigo,
-                tertiary: colors.pink,
-                success: colors.green,
-                danger: colors.rose,
-                warning: colors.yellow,
-                info: colors.cyan,
-                muted: colors.gray
-            },
-            fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans]
-            }
-        }
     }
 }
