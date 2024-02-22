f0f8c883aa47e4f43892aeea0b5b1251722351a6
Chuck <chuck@eclecticsaddlebag.com>
2024-02-22 09:55:37 -0500

    Hide secret key in vcr output

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -18,6 +18,7 @@ module VCRSetup
       c.hook_into :webmock
       c.allow_http_connections_when_no_cassette = true
       c.ignore_localhost = true
+      c.filter_sensitive_data("<MY_SECRET_KEY>") { Rails.application.credentials.dig(:maxmind_api_key) }
     end
   end
 end
