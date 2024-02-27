f7f4c91009337be869d4aef0046e2b6ff5e73465
Chuck <chuck@eclecticsaddlebag.com>
2024-02-27 10:42:46 -0500

    Update esbuild configuration

---
+++ app/javascript/controllers/activation_controller.js
@@ -0,0 +1,27 @@
+import {Controller} from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ["types", "roles", "venue"]
+
+    connect() {
+        console.log("Map Controller Connected")
+    }
+
+    update(event) {
+        const eventType = event.target.value
+        const roles = this.rolesTarget
+        const venue = this.venueTarget
+        const roleOptions = Array.from(roles.options)
+        const venueOptions = Array.from(venue.options)
+        const optionToFind = roleOptions.find(option => option.text === "Member")
+        const venueToFind = venueOptions.find(option => option.text === "Salt and Light Center")
+
+        eventType === "weekend" ? venueToFind.selected = true : venue.selectedIndex = 0
+
+        if (eventType === "closing" || eventType === "candlelight" || eventType === "sendoff") {
+            optionToFind.selected = true
+        } else {
+            roles.selectedIndex = 0
+        }
+    }
+}
