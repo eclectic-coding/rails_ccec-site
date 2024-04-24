974fe40d82765895855ea89676c0088dfebc8eb4
Chuck <chuck@eclecticsaddlebag.com>
2024-04-24 07:06:36 -0400

    Update footer events view component

---
+++ app/views/components/footer/events/component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Footer::Events::Component < ApplicationViewComponent
+  option :events
+end
--- app/views/components/footer/events_component.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Footer::EventsComponent < ApplicationViewComponent
-  option :events
-end
