14e760a9bbe1fb72d643c7cc79e8133e2c9ed2d5
Chuck <chuck@eclecticsaddlebag.com>
2024-04-27 12:11:40 -0400

    Linting fixes

--- spec/views/components/event/address_component_spec.rb
+++ spec/views/components/event/address_component_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Event::Address::Component, type: :component do
     event = create(:event, event_type: :gathering, address: create(:address))
 
     # Stub the method responsible for making the external API call
-    allow_any_instance_of(Address).to receive(:geocode).and_return([1,1])
+    allow_any_instance_of(Address).to receive(:geocode).and_return([1, 1])
 
     render_inline(described_class.new(event: event))
 
