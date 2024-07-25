7f637c578ac3f9c81f2c259426770b1fd0de61ef
Chuck <chuck@eclecticsaddlebag.com>
2024-07-25 16:16:32 -0400

    Fix destroy booking test

--- spec/system/prayer_admin/prayer_admin_destroy_bookings_spec.rb
+++ spec/system/prayer_admin/prayer_admin_destroy_bookings_spec.rb
@@ -3,24 +3,25 @@ require 'system_helper'
 RSpec.describe "PrayerAdmin::DestroyBookings", type: :system do
 
   describe 'destroy booking' do
-    let(:event) { create(:event, :weekend) }
-    let(:prayer_vigil) { PrayerVigil.find_by(walk_number: event.walk_number) }
-    let(:prayer_slot) { PrayerSlot.find_by(prayer_vigil_id: prayer_vigil.id) }
-    let(:booking) { create(:booking, prayer_vigil_id: prayer_vigil.id, prayer_slot_id: prayer_slot.id) }
+    before do
+      login_as(create(:user, :admin))
+      @event = create(:event, :weekend)
+      @prayer_vigil = PrayerVigil.find_by(walk_number: @event.walk_number)
+      @prayer_slot = PrayerSlot.find_by(prayer_vigil_id: @prayer_vigil.id)
+      @booking = create(:booking, prayer_vigil_id: @prayer_vigil.id, prayer_slot_id: @prayer_slot.id)
+    end
 
     it 'destroys booking' do
-      login_as(create(:user, :prayer))
-      visit prayer_admin_prayer_vigils_path
-      take_screenshot
+      visit prayer_admin_prayer_vigil_path(@prayer_vigil)
 
-      expect(page).to have_content(booking.user.full_name)
+      expect(page).to have_content(@booking.full_name)
 
       accept_confirm do
         click_on 'Remove'
       end
 
-      expect(page).to have_content('Booking has been cancelled')
-      expect(page).not_to have_content(booking.user.full_name)
+      expect(page).to have_content('Reservation was successfully removed and the user has been notified.')
+      expect(page).not_to have_content(@booking.full_name)
     end
   end
 end
